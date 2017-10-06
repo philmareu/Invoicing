300844caecc3497e3f426e48ead2bcc381f45fee
Phil Mareu <philmareu@gmail.com>
2017-10-06 14:34:11 -0500

    laravel default auth scaffolding

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ Route::get('api/clients', 'Endpoints\ClientsEndpointController@get');
 Route::post('api/clients', 'Endpoints\ClientsEndpointController@store');
 Route::put('api/clients/{client}', 'Endpoints\ClientsEndpointController@update');
 Route::delete('api/clients/{client}', 'Endpoints\ClientsEndpointController@destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
